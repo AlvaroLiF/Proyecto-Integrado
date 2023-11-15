da38edbcb6c3d8df34ef1a25de40d43d3a1d6b7d
AlvaroLiF <alvarolimonflor@gmail.com>
2023-11-15 16:00:30 +0100

    Proyecto

--- Componentx_Backend/routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authenticateUser = require('../middleware/auth');
-const OrderController = require('../controllers/orderController');
-
-router.get('/orders', authenticateUser, OrderController.getOrderHistory);
-router.post('/orders/place', authenticateUser, OrderController.placeOrder);
-
-module.exports = router;
